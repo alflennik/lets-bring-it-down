ce4d46046cf6f63559db5de24a0c986cc85b1938
Alex Flenniken <alex.flenniken@whiteops.com>
2020-03-28 12:34:59 -0400

    move raw data to server

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,9 @@
 const fs = require('fs').promises;
 const functions = require('firebase-functions');
+const rawData = require('./rawData')
 
 exports.app = functions.https.onRequest(async (req, res) => {
   const indexHtml = await fs.readFile('./index.html', 'utf-8')
-  res.send(indexHtml);
+  const indexHtmlWithData = indexHtml.replace('RAW_DATA_FROM_SERVER', '`' + JSON.stringify(rawData) + '`')
+  res.send(indexHtmlWithData);
 });
